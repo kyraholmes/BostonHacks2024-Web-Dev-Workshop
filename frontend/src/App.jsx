393c836269ff8f570715f2d3351c6ddf9a28dfc7
adam-godel <agodel@bu.edu>
2024-10-25 10:14:59 -0400

    Changed site title

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,7 +3,7 @@ import './App.css'
 function App() {
   return (
     <div id="chat">
-      <h1>BostonHacks Fall 2024</h1>
+      <h1>BostonHacks 2024 Workshop</h1>
       <form onSubmit={onSubmit}>
         <h2>Ask Me A Question</h2>
         <input type="text" name='user-input' id="question-input" placeholder="What would you like to ask?" />
